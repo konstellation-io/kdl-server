87c58e5b0717830e867cc0cf5dbb67aa0d6775b9
marco.gattelli <marco.gattelli@intelygenz.com>
2021-02-04 16:22:29 +0100

    feat: Adjust back button behavior when is electron

--- app/ui/src/Pages/Home/pages/Server/components/ServerBar/ServerBar.tsx
+++ app/ui/src/Pages/Home/pages/Server/components/ServerBar/ServerBar.tsx
@@ -4,22 +4,37 @@ import React, { useEffect } from 'react';
 import SettingsMenu from '../SettingsMenu/SettingsMenu';
 import styles from './ServerBar.module.scss';
 import { useForm } from 'react-hook-form';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 import useProjectFilters from 'Pages/Home/apollo/hooks/useProjectFilters';
 import ArrowsNavigator from './components/ArrowsNavigator/ArrowsNavigator';
 import Breadcrumbs from './components/Breadcrumbs/Breadcrumbs';
+import ROUTE from 'Constants/routes';
 
 type FormData = {
   projectName: string;
 };
 
+const isElectron = /electron/i.test(navigator.userAgent);
+
 function ServerBar() {
   const { goBack, goForward } = useHistory();
+  const isHome = useRouteMatch({
+    path: ROUTE.HOME,
+    strict: true,
+  });
   const { updateFilters } = useProjectFilters();
   const { setValue, unregister, register, watch } = useForm<FormData>({
     defaultValues: { projectName: '' },
   });
 
+  function getBackBehavior() {
+    let handleGoBack = goBack;
+    if (isElectron && isHome?.isExact) {
+      const { ipcRenderer } = window.require('electron');
+      handleGoBack = () => ipcRenderer.send('closeServer');
+    }
+    return handleGoBack;
+  }
   useEffect(() => {
     register('projectName');
     return () => unregister('projectName');
@@ -35,7 +50,10 @@ function ServerBar() {
   return (
     <div className={styles.container}>
       <Left className={styles.left}>
-        <ArrowsNavigator onBackClick={goBack} onForwardClick={goForward} />
+        <ArrowsNavigator
+          onBackClick={getBackBehavior()}
+          onForwardClick={goForward}
+        />
         <Breadcrumbs />
       </Left>
       <Right className={styles.right}>

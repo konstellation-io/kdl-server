1ca70bee16daba07dbfc7d90c1f19672ad8cef0b
Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>
2021-02-10 12:39:24 +0100

    Move some score code to utils file

--- app/ui/src/Pages/NewProject/pages/Information/components/DescriptionScore/DescriptionScore.tsx
+++ app/ui/src/Pages/NewProject/pages/Information/components/DescriptionScore/DescriptionScore.tsx
@@ -1,30 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import styles from './DescriptionScore.module.scss';
-import cx from 'classnames';
+import { ScoreLevels, getScoreLevel, levels, scoreText } from './DescriptionScoreUtils';
 
-enum ScoreLevels {
-  LOW = 'LOW',
-  MEDIUM = 'MEDIUM',
-  HIGH = 'HIGH',
-  VERY_HIGH = 'VERY_HIGH',
-}
+import cx from 'classnames';
+import styles from './DescriptionScore.module.scss';
 
 type Props = {
   score: number;
 };
-
-const levels = Object.keys(ScoreLevels);
 
 function DescriptionScore({ score }: Props) {
   const [scoreLevel, setScoreLevel] = useState(ScoreLevels.LOW);
   const [scoreIndex, setScoreIndex] = useState(0);
 
   useEffect(() => {
-    let selectedLevel = ScoreLevels.LOW;
-    if (score >= 25 && score < 50) selectedLevel = ScoreLevels.MEDIUM;
-    else if (score >= 50 && score < 75) selectedLevel = ScoreLevels.HIGH;
-    else if (score >= 75) selectedLevel = ScoreLevels.VERY_HIGH;
-
+    let selectedLevel = getScoreLevel(score);
     const selectedScoreIndex = levels.indexOf(selectedLevel);
 
     setScoreLevel(selectedLevel);
@@ -42,26 +31,13 @@ function DescriptionScore({ score }: Props) {
     ));
   }
 
-  function renderHintLabel() {
-    switch (scoreLevel) {
-      case ScoreLevels.LOW:
-        return 'Description is incomplete for Knowledge Graph.';
-      case ScoreLevels.MEDIUM:
-        return 'Please, write some more text in description.';
-      case ScoreLevels.HIGH:
-        return 'Your description is good enough to be indexed.';
-      case ScoreLevels.VERY_HIGH:
-        return 'Nice, your description is fully indexable.';
-    }
-  }
-
   return (
     <div className={styles.container}>
       <span className={cx(styles.scoreLabel, styles[scoreLevel])}>
         {score}%
       </span>
       <div className={styles.scoreLevels}>{renderScoreLevels()}</div>
-      <span className={styles.hintLabel}>{renderHintLabel()}</span>
+      <span className={styles.hintLabel}>{scoreText.get(scoreLevel)}</span>
     </div>
   );
 }

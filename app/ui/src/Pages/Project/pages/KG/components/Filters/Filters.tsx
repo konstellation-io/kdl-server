9bf411d86acfcf038b5a1e0384a98e59782555be
marco.gattelli <marco.gattelli@intelygenz.com>
2021-02-15 09:53:11 +0100

    PR fixes

--- app/ui/src/Pages/Project/pages/KG/components/Filters/Filters.tsx
+++ app/ui/src/Pages/Project/pages/KG/components/Filters/Filters.tsx
@@ -2,21 +2,24 @@ import React, { useState } from 'react';
 import styles from './Filters.module.scss';
 import ScoreFilter from './components/ScoreFilter/ScoreFilter';
 
+const MAX_SCORE = 100;
+
 type Props = {};
 function Filters({}: Props) {
-  const max = 100;
-  const [scores, setScores] = useState<number[]>([20, 80]);
-  const topScore = max - scores[0];
-  const bottomScore = max - scores[1];
+  const [scores, setScores] = useState<[number, number]>([20, 80]);
 
   const handleSliderChange = (
     _: React.ChangeEvent<{}>,
     numbers: number | number[]
-  ) => setScores(numbers as number[]);
+  ) => setScores(numbers as [number, number]);
 
   return (
     <div className={styles.container}>
-      <ScoreFilter scores={scores} onChange={handleSliderChange} max={max} />
+      <ScoreFilter
+        scores={scores}
+        onChange={handleSliderChange}
+        max={MAX_SCORE}
+      />
       <div className={styles.topic}>TOPIC</div>
     </div>
   );

e374e8294a7e576f9d70775bc0b292dda51e838b
Vasja Urbancic <vasja.urbancic@intelygenz.com>
2021-03-11 17:10:41 +0100

    Basic unit tests for DescriptionEvaluator

---
+++ kg/tests/unit/test_description_evaluator.py
@@ -0,0 +1,46 @@
+import unittest
+
+from src.description_evaluator import DescriptionEvaluator
+
+
+class TestDescriptionEvaluator(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.evaluator = DescriptionEvaluator()
+
+    def test_get_description_quality_returns_value_between_0_and_1(self):
+
+        query = "This is my test query"
+
+        result = self.evaluator.get_description_quality(query)
+
+        self.assertGreaterEqual(result, 0)
+        self.assertLessEqual(result, 1)
+
+    def test_get_description_quality_with_nonstring_input_raises_error(self):
+
+        queries = [None, 3, True]
+        for query in queries:
+            with self.assertRaises(ValueError):
+                self.evaluator.get_description_quality(description=query)
+
+    def test_get_description_quality_with_empty_string_returns_zero(self):
+
+        query = ""
+        expect = 0
+        result = self.evaluator.get_description_quality(description=query)
+
+        self.assertEqual(expect, result)
+
+    def test_get_description_quality_with_long_nonsense_string_returns_zero(self):
+
+        query = "lkajsdo9erugnfklsdnfwporweournzksdf'sl;dfqwpsokwerpijrfowlds;wladkowwpfm3"
+        expect = 0
+        result = self.evaluator.get_description_quality(description=query)
+
+        self.assertEqual(expect, result)
+
+
+if __name__ == "__main__":
+
+    unittest.main()

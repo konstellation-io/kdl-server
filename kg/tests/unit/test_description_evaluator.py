6e3967e8f3f899cb359d1636c29262199fe445a4
Vasja Urbancic <vasja.urbancic@intelygenz.com>
2021-03-12 13:44:32 +0100

    Add a mocked Recommender to DescriptionEvaluator unit tests, and a new unit test for high-quality description

--- kg/tests/unit/test_description_evaluator.py
+++ kg/tests/unit/test_description_evaluator.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import MagicMock
 
 from description_evaluator import DescriptionEvaluator
 
@@ -6,7 +7,13 @@ from description_evaluator import DescriptionEvaluator
 class TestDescriptionEvaluator(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.evaluator = DescriptionEvaluator()
+        mock_paper = MagicMock(score=0.8)
+        top_items = MagicMock()
+        top_items.__getitem__.return_value = mock_paper  # mocking last_item_score = float(top_items.items[-1].score)
+        recommender = MagicMock()
+        recommender.get_top_items.return_value = top_items
+
+        self.evaluator = DescriptionEvaluator(recommender=recommender)
 
     def test_get_description_quality_returns_value_between_0_and_1(self):
 
@@ -41,6 +48,19 @@ class TestDescriptionEvaluator(unittest.TestCase):
         result = self.evaluator.get_description_quality(description=query)
 
         self.assertEqual(expect, result)
+
+    def test_get_description_quality_with_high_quality_description_returns_quality_above_50_percent(self):
+
+        query = """The purpose of this project is to generate an algorithm for automated image quality enhancement.
+            We have an extensive dataset of paired images of the same scene, in which one of the pair of images was taken
+            with a high-end professional DSLR camera and another pair was taken with a mobile phone camera. On the basis of
+            these paired images, we want to generate a neural network algorithm that is capable of enhancing the quality
+            of any mobile phone photograph, irrespective of the optical properties of the source camera. We want the
+            solution to achieve various aims, including denoising, color and contrast improvement and removal of Moire
+            patterns."""
+        result = self.evaluator.get_description_quality(description=query)
+
+        self.assertGreaterEqual(result, 0.5)
 
 
 if __name__ == "__main__":

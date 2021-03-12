473be8efb69043c8936f5c0622bc1a072c5fad1d
Vasja Urbancic <vasja.urbancic@intelygenz.com>
2021-03-12 12:17:11 +0100

    Use the returned recommender hits to evaluate description quality.

--- kg/src/description_evaluator.py
+++ kg/src/description_evaluator.py
@@ -1,10 +1,25 @@
+import numpy as np
+
+import config
+from recommender import Recommender
+from tools.assets import AssetLoader
+
+
 class DescriptionEvaluator(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, recommender):
+        self.recommender = recommender
 
     @staticmethod
-    def get_description_quality(description: str) -> float:
+    def convert_score_to_quality(score):
+        a = 2.5    # estimated empirically, see notebook 18
+        b = -1.25  # ibid
+        quality = a * score + b
+        quality = np.clip(quality, a_min=0, a_max=1)
+
+        return quality
+
+    def get_description_quality(self, description: str) -> float:
 
         if not isinstance(description, str):
             raise ValueError("`get_description_quality` expected a string input.")
@@ -13,6 +28,26 @@ class DescriptionEvaluator(object):
 
         if n_words < 10:
             return 0
-        if n_words > 512:
-            return 1
-        return n_words / 512
+
+        else:
+            rec_list = self.recommender.get_top_items(description, n_hits=100)
+            last_item_score = rec_list.items[-1].score
+            quality = self.convert_score_to_quality(last_item_score)
+            return quality
+
+
+if __name__ == "__main__":
+    # TODO: Remove main and move example to tests
+    # Set up recommender to inject into DescriptionEvaluator
+    assets = AssetLoader(config.ASSET_ROUTE)
+    recommender = Recommender(
+        model=assets.model,
+        tokenizer=assets.tokenizer,
+        vectors=assets.vectors,
+        dataset=assets.dataset)
+
+    # Instantiate the DescriptionEvaluator and get description quality
+    de = DescriptionEvaluator(recommender=recommender)
+    example_descr = "A test query for some machine learning project using image classification. "
+    estimated_quality = de.get_description_quality(example_descr)
+

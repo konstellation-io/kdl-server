a68c218d8179351ee8193632f8eabeb81de2e073
Vasja Urbancic <vasja.urbancic@intelygenz.com>
2021-03-15 11:15:03 +0100

    Remove the description evaluator test with a high quality description input from unit tests (requires true not mocked recommender) and rename the test class to clarify it uses mocked recommender outputs

--- kg/tests/unit/test_description_evaluator.py
+++ kg/tests/unit/test_description_evaluator.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock
 from description_evaluator import DescriptionEvaluator
 
 
-class TestDescriptionEvaluator(unittest.TestCase):
+class TestDescriptionEvaluatorWithMockedRecommender(unittest.TestCase):
 
     def setUp(self) -> None:
         mock_paper = MagicMock(score=0.8)
@@ -48,19 +48,6 @@ class TestDescriptionEvaluator(unittest.TestCase):
         result = self.evaluator.get_description_quality(description=query)
 
         self.assertEqual(expect, result)
-
-    def test_get_description_quality_with_high_quality_description_returns_quality_above_50_percent(self):
-
-        query = """The purpose of this project is to generate an algorithm for automated image quality enhancement.
-            We have an extensive dataset of paired images of the same scene, in which one of the pair of images was
-            taken with a high-end professional DSLR camera and another pair was taken with a mobile phone camera.
-            On the basis of these paired images, we want to generate a neural network algorithm that is capable of
-            enhancing the quality of any mobile phone photograph, irrespective of the optical properties of the
-            source camera. We want the solution to achieve various aims, including denoising, color and contrast
-            improvement and removal of Moire patterns."""
-        result = self.evaluator.get_description_quality(description=query)
-
-        self.assertGreaterEqual(result, 0.5)
 
 
 if __name__ == "__main__":

79605e82e8d53b8efca8c1a2649ec02a76ce4793
Vasja Urbancic <vasja.urbancic@intelygenz.com>
2021-03-24 12:41:50 +0100

    Change gen_inputs fixture to return list[str] instead of a numpy string array, as numpy arrays are refused by the pretrained tokenizer

--- kg/tests/conftest.py
+++ kg/tests/conftest.py
@@ -1,12 +1,13 @@
 from collections import OrderedDict
+from typing import List
 
-import numpy as np
 import pytest
 from faker import Faker
 
 
 @pytest.fixture
-def gen_inputs(n_sentences: int = 10, n_paragraph: int = 3, n_inputs: int = 4) -> np.ndarray:
+def gen_inputs(n_sentences: int = 5, n_paragraph: int = 2, n_inputs: int = 4) -> List[str]:
+    """Creates garbled texts that can be used as inputs for testing tokenization and vectorization tasks"""
     locales = OrderedDict([('en-US', 1)])
     Faker.seed(0)
     fake = Faker(locales)
@@ -18,4 +19,4 @@ def gen_inputs(n_sentences: int = 10, n_paragraph: int = 3, n_inputs: int = 4) -
 
         outputs.append(description)
 
-    return np.array(outputs)
+    return outputs

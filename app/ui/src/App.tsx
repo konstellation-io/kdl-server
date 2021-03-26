cff7a915f6a3c8277310290240c13528760248fc
marco.gattelli <marco.gattelli@intelygenz.com>
2021-03-26 12:44:37 +0100

    remove api token page

--- app/ui/src/App.tsx
+++ app/ui/src/App.tsx
@@ -43,7 +43,6 @@ function Routes() {
         <Route exact path={ROUTE.USERS} component={Users} />
         <Route exact path={ROUTE.NEW_USER} component={NewUser} />
         <Route exact path={ROUTE.USER_SSH_KEY} component={UserSshKey} />
-        <Route exact path={ROUTE.USER_API_TOKENS} component={UserApiTokens} />
 
         <Route exact path={ROUTE.NEW_PROJECT} component={NewProject} />
         <Route

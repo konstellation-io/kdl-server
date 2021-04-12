9624fdda399417c605ce38c218e4389f5b23bc71
marco gattelli <mgattelligit@gmail.com>
2021-04-12 12:07:39 +0200

    remove unusefull aaa

--- app/ui/src/Components/DescriptionScore/DescriptionScore.test.tsx
+++ app/ui/src/Components/DescriptionScore/DescriptionScore.test.tsx
@@ -84,9 +84,6 @@ describe('DescriptionScore utils', () => {
 
   describe('scoreText', () => {
     it('should match snapshot', () => {
-      // Arrange.
-      // Act.
-      // Assert.
       expect(scoreText).toMatchSnapshot();
     });
   });

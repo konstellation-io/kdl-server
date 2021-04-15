a3157f539e5a9ae7397581f6cd99268d60b6d5b5
Marco <mgattelligit@gmail.com>
2021-04-15 13:06:24 +0200

    Feat: Add bulk actions to project members.
    * feat: Now you can manage members with bulk actions
    
    * update schema
    
    * add confirm modals
    
    * implement bulk actions on project members in backend
    
    * PR fixes
    
    * fix mongo UpdateMembersAccessLevel query to be executed with several users at the same time
    
    * change UpdateMany to UpdateOne in RemoveMembers function
    
    * change UpdateMembersAccessLevel from using FindOneAndUpdate to UpdateOne
    
    * change checkAtLeastOneAdmin function to reduce complexity
    
    * Small visual changes
    
    * Fix z-index issue on project panels
    
    * Visual updates on member confirmation rows
    
    * Sonar fixes
    
    Co-authored-by: Angel <angel.tabar@intelygenz.com>
    Co-authored-by: Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>

---
+++ app/ui/src/Graphql/mutations/types/UpdateMembers.ts
@@ -0,0 +1,38 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { UpdateMembersInput, AccessLevel } from './../../types/globalTypes';
+
+// ====================================================
+// GraphQL mutation operation: UpdateMembers
+// ====================================================
+
+export interface UpdateMembers_updateMembers_members_user {
+  __typename: 'User';
+  id: string;
+  email: string;
+  lastActivity: string | null;
+}
+
+export interface UpdateMembers_updateMembers_members {
+  __typename: 'Member';
+  user: UpdateMembers_updateMembers_members_user;
+  accessLevel: AccessLevel;
+  addedDate: string;
+}
+
+export interface UpdateMembers_updateMembers {
+  __typename: 'Project';
+  id: string;
+  members: UpdateMembers_updateMembers_members[];
+}
+
+export interface UpdateMembers {
+  updateMembers: UpdateMembers_updateMembers;
+}
+
+export interface UpdateMembersVariables {
+  input: UpdateMembersInput;
+}

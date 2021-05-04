31b296a038e08a40594e9af1b9a403d6118a3ea0
marco gattelli <mgattelligit@gmail.com>
2021-05-04 15:55:57 +0200

    change the text

--- app/ui/src/Pages/NewProject/pages/SidebarComponents/Repository/SidebarRepository.tsx
+++ app/ui/src/Pages/NewProject/pages/SidebarComponents/Repository/SidebarRepository.tsx
@@ -11,8 +11,8 @@ const SidebarRepository = () => (
         outside the server.
       </li>
       <li>
-        <strong>Internal repo:</strong> use a version-control system that will
-        be deployed inside the current server.
+        <strong>Internal repo:</strong> use a version-control system deployed
+        inside the current server.
       </li>
     </ol>
   </div>

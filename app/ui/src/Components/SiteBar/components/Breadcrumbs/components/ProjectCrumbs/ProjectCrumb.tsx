735f5e3ae58a08e2565e6aed25869f49e9017132
marco gattelli <mgattelligit@gmail.com>
2021-05-14 08:44:47 +0200

    refactor Crumbs

---
+++ app/ui/src/Components/SiteBar/components/Breadcrumbs/components/ProjectCrumbs/ProjectCrumb.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useReactiveVar } from '@apollo/client';
+import useProjectNavigation from 'Hooks/useProjectNavigation';
+import Crumb, { BottomComponentProps } from '../Crumb/Crumb';
+import NavigationSelector from '../../../NavigationSelector/NavigationSelector';
+import { openedProject } from 'Graphql/client/cache';
+import { useLocation } from 'react-router-dom';
+
+function ProjectCrumb() {
+  const location = useLocation();
+  const project = useReactiveVar(openedProject);
+
+  const { allRoutes } = useProjectNavigation(project?.id || '');
+
+  const lastParam = location.pathname.split('/').pop();
+  const projectRoute = allRoutes.find(({ id }) => id === lastParam);
+
+  if (!projectRoute || !project) return null;
+
+  const { label, Icon } = projectRoute;
+  return (
+    <Crumb
+      crumbText={label}
+      LeftIconComponent={<Icon className="icon-small" />}
+      BottomComponent={(props: BottomComponentProps) => (
+        <NavigationSelector options={allRoutes} {...props} />
+      )}
+    />
+  );
+}
+
+export default ProjectCrumb;
---
+++ app/ui/src/Components/SiteBar/components/Breadcrumbs/components/ProjectsCrumbs/ProjectsCrumb.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useQuery, useReactiveVar } from '@apollo/client';
+import Crumb, { BottomComponentProps } from '../Crumb/Crumb';
+import { GetProjects } from 'Graphql/queries/types/GetProjects';
+import GetProjectsQuery from 'Graphql/queries/getProjects';
+import { openedProject } from 'Graphql/client/cache';
+import ProjectIcon from 'Components/Icons/ProjectIcon/ProjectIcon';
+import ProjectSelector from '../../../ProjectSelector/ProjectSelector';
+
+function ProjectsCrumb() {
+  const { data, loading, error } = useQuery<GetProjects>(GetProjectsQuery);
+  const project = useReactiveVar(openedProject);
+
+  if (loading || !project || !data) return null;
+  if (error) throw Error('cannot retrieve data at useBreadcrumbs');
+
+  const activeProjects = data.projects.filter(
+    (p) => !p.archived && !p.needAccess
+  );
+
+  return (
+    <Crumb
+      crumbText={project.name}
+      LeftIconComponent={
+        <ProjectIcon className="icon-regular" archived={project.archived} />
+      }
+      BottomComponent={(props: BottomComponentProps) => (
+        <ProjectSelector options={activeProjects} {...props} />
+      )}
+    />
+  );
+}
+
+export default ProjectsCrumb;

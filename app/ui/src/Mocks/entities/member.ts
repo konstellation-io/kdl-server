525df94aaca823299d5c031c9bce8b0ed8528a56
Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>
2021-05-24 17:07:18 +0200

    use AccessLevel enum instead of raw strings

--- app/ui/src/Mocks/entities/member.ts
+++ app/ui/src/Mocks/entities/member.ts
@@ -1,22 +1,23 @@
 import { newUser, user1, user2 } from './user';
+import { AccessLevel } from '../../Graphql/types/globalTypes';
 
 export const member1 = {
   user: user1,
-  accessLevel: 'ADMIN',
+  accessLevel: AccessLevel.ADMIN,
   addedDate: '2020-02-02',
   __typename: 'Member',
 };
 
 export const member2 = {
   user: user2,
-  accessLevel: 'MANAGER',
+  accessLevel: AccessLevel.MANAGER,
   addedDate: '2020-02-03',
   __typename: 'Member',
 };
 
 export const newMember = {
   user: newUser,
-  accessLevel: 'MANAGER',
+  accessLevel: AccessLevel.MANAGER,
   addedDate: '2020-02-03',
   __typename: 'Member',
 };

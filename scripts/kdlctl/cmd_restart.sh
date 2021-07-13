132966d74f43ef9426069790b2cacffd004ab8f0
Angel Sanchez Roa <angel.sanchez@intelygenz.com>
2021-07-13 13:38:24 +0200

    fix restart command

--- scripts/kdlctl/cmd_restart.sh
+++ scripts/kdlctl/cmd_restart.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
 cmd_restart() {
-    TYPE=${1:-"microk8s"}
+    TYPE=${1:-"kdl"}
 
     if [ "$TYPE" = "kdl" ]; then
-      restart_admin_pods
-    fi
-
-    if [ "$TYPE" = "version" ]; then
-      shift
-      restart_version "$@"
+      restart_kdl_pods
     fi
 
     if [ "$TYPE" = "microk8s" ]; then
@@ -22,16 +17,17 @@ show_restart_help() {
   echo "$(help_global_header "restart <type> [options]")
 
     types:
-      minikube  restarts minikube (default option).
-      kdl       restarts pods on kdl namespace.
-      version   <runtime-name> <version-name> restarts all pods inside a version.
+      microk8s  restarts microk8s.
+      kdl       restarts pods on kdl namespace (default option).
 
     $(help_global_options)
 "
 }
 
-restart_admin_pods() {
-  POD_NAMES=$(kubectl -n "${NAMESPACE}" get pod -l type=admin -o custom-columns=":metadata.name" --no-headers | tr '\n' ' ')
+restart_kdl_pods() {
+  microk8s_kubeconfig
+
+  POD_NAMES=$(kubectl -n "${NAMESPACE}" get pod -o custom-columns=":metadata.name" --no-headers | tr '\n' ' ')
 
   if [ -z "$POD_NAMES" ]; then
     echo_fatal "no pods to restart"
@@ -39,29 +35,6 @@ restart_admin_pods() {
   fi
 
   echo_wait "Restarting kdl pods"
-  # shellcheck disable=SC2086 # this behaviour is expected here
-  run kubectl -n "${NAMESPACE}" delete pod ${POD_NAMES} --grace-period=0
-}
-
-get_version_pods() {
-  NAMESPACE=$1
-  VERSION=$2
-  kubectl -n "$NAMESPACE" get pod -l version-name="$VERSION" -o custom-columns=":metadata.name" --no-headers
-}
-
-restart_version() {
-  NAME=$1
-  VERSION=$2
-  NAMESPACE="kdl-${NAME}"
-
-  POD_NAMES=$(get_version_pods "$NAMESPACE" "$VERSION" | tr '\n' ' ')
-
-  if [ -z "$POD_NAMES" ]; then
-    echo_fatal "no pods to restart"
-    return
-  fi
-
-  echo_wait "Restarting '$VERSION' on $NAME"
   # shellcheck disable=SC2086 # this behaviour is expected here
   run kubectl -n "${NAMESPACE}" delete pod ${POD_NAMES} --grace-period=0
 }

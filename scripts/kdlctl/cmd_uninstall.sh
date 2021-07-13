6022cef4600462f130d2fbf0ac1b858e67e439e5
Angel Sanchez Roa <angel.sanchez@intelygenz.com>
2021-07-13 11:44:37 +0200

    mk8s uninstall

--- scripts/kdlctl/cmd_uninstall.sh
+++ scripts/kdlctl/cmd_uninstall.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 cmd_uninstall() {
+  microk8s_kubeconfig
+
   while true; do
     read -p "⚠️ Do you wish to uninstall KDL? CAUTION: all data will be permanently deleted. (y/n) " yn
     case $yn in
@@ -8,8 +10,8 @@ cmd_uninstall() {
       echo_info "Deleting \"$RELEASE_NAME\" helm release..."
       helm delete "$RELEASE_NAME" -n $NAMESPACE
 
-      echo_info "Deleting all generated data in the hostpath-provisioner..."
-      minikube -p "$MINIKUBE_PROFILE" ssh -- sudo rm -rf /tmp/hostpath-provisioner/\*
+#      echo_info "Deleting all generated data in the hostpath-provisioner..."
+#      minikube -p "$MINIKUBE_PROFILE" ssh -- sudo rm -rf /tmp/hostpath-provisioner/\*
 
       echo_info "Deleting \"$NAMESPACE\" k8s namespace..."
       kubectl delete ns $NAMESPACE --force --grace-period 0

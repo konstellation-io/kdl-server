67d0a67ee016a84eccc4cccf1e14e3c625a5e4e7
Alejandro Aceituna Cano <dev@aacecan.com>
2021-08-11 13:23:59 +0200

    Take into account if Free Desktop env variables are set

--- scripts/create_self_signed_cert.sh
+++ scripts/create_self_signed_cert.sh
@@ -42,6 +42,13 @@ fi
 TRUST_STORES=nss .certs/mkcert --install  *.$DOMAIN
 mv _wildcard.* .certs/toolkit
 
+
+if [ ! -z ${XDG_DATA_HOME} ]; then
+    CA_FILE=${XDG_DATA_HOME}/mkcert/rootCA.pem
+else
+    CA_FILE=${HOME}/.local/share/mkcert/rootCA.pem
+fi
+
 echo "Creating K8S secrets with the CA private keys"
 kubectl -n $NAMESPACE create secret tls $DOMAIN-tls-secret --key=$CA_CERTS_FOLDER/_wildcard.$DOMAIN-key.pem --cert=$CA_CERTS_FOLDER/_wildcard.$DOMAIN.pem --dry-run -o yaml | kubectl apply -f -
 kubectl -n $NAMESPACE create secret generic mkcert-ca --from-file=mkcert-ca.crt=${HOME}/.local/share/mkcert/rootCA.pem --dry-run -o yaml | kubectl apply -f -

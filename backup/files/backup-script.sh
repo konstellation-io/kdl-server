c31f50e8f641baa7b9c87e5dcfb72d46ff9873cf
Alejandro Aceituna Cano <dev@aacecan.com>
2021-11-19 14:24:12 +0100

    feat(backup): Backup performing OK

--- backup/files/backup-script.sh
+++ backup/files/backup-script.sh
@@ -4,26 +4,26 @@ DATE=$(date +%Y%m%d)
 mkdir -p /backup/{gitea,mongodb,mlflow,kubernetes}
 
 # Gitea backup
-gitea_backup (){
+function gitea_backup (){
     echo "${DB_HOST}:${POSTGRES_DB}:${DB_USER}:${POSTGRES_PASSWORD}" > ~/.pgpass
     chmod 600 ~/.pgpass
     tar zcvf /backup/gitea/data.tar.gz --directory '/data' --exclude './gitea/indexers' .
     pg_dump -h "$(echo ${DB_HOST} | cut -f 1 -d ":" )" -p "$(echo ${DB_HOST} | cut -f 2 -d ":" )" -U "${DB_USER}" -v -F c -f /backup/gitea/postgres_gitea.dump "${POSTGRES_DB}"
 }
 # MongoDB backup
-mongobackup () { 
+function mongobackup () {
     mongodump --host "${MONGO_HOST}" --port "${MONGO_PORT}" --username "${MONGO_INITDB_ROOT_USERNAME}" --password "${MONGO_INITDB_ROOT_PASSWORD}" --out /backup/mongodb
 }
 # MLFlow backup
 
-mlflow_backup (){
+function mlflow_backup (){
     for i in /shared-volume/*/mlflow.db; do
         path="/backup/mlflow/";
         cp --preserve -R "${i%%mlflow.db}" "${path}"; done
 }
 
 # Kubernetes CR and Secrets backup
-kubernetes_backup(){
+function kubernetes_backup (){
     kubectl --token "$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" get secrets -n kdl | \
         awk '/ssh-keys/{print $1}' | \
         xargs kubectl --token "$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" get secrets -n kdl -o yaml > /backup/kubernetes/ssh-secrets.yaml
@@ -37,14 +37,16 @@ gitea_backup
 echo "Performing MONGO backup..."
 mongobackup
 
-echo "Performing MLFLOW backup..."
-mlflow_backup
+if [ -d "/shared-storage/" ]; then
+    echo "Performing MLFLOW backup..."
+    mlflow_backup
+fi
 
 echo "Performing KUBERNETES backup..."
 kubernetes_backup
 
 # Compress and send to AWS
 echo "Compressing backup..."
-tar zcvf /backup.tar.gz /backup
+tar zcvf "${HOME}/backup.tar.gz" /backup
 echo "Sending backup..."    
-aws s3 cp "/backup.tar.gz" "s3://${BUCKET_NAME}/backup_${DATE}.tar.gz"
+aws s3 cp "${HOME}/backup.tar.gz" "s3://${BUCKET_NAME}/backup_${DATE}.tar.gz"

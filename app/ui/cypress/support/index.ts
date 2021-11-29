20d0264efa6e04949825b708cdde25be8169ab95
Juan Jesús Padrón Hernández <juanjesus.padron@intelygenz.com>
2021-11-29 16:39:15 +0000

    update cypress and fix tests

--- app/ui/cypress/support/index.ts
+++ app/ui/cypress/support/index.ts
@@ -1,5 +1,7 @@
-Cypress.Commands.add('kstInterceptor', (operation, responseObject) => {
-  cy.intercept('/api/query', (req) => {
+import Chainable = Cypress.Chainable;
+
+Cypress.Commands.add('kstInterceptor', (operation, responseObject): Chainable => {
+  return cy.intercept('/api/query', (req) => {
     const { operationName } = req.body;
     if (operationName === operation) req.reply(responseObject);
   });
@@ -7,6 +9,8 @@ Cypress.Commands.add('kstInterceptor', (operation, responseObject) => {
 
 Cypress.Commands.add('getByTestId', (dataTestId) => cy.get(`[data-testid="${dataTestId}"]`));
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 Cypress.Commands.add('findByTestId', { prevSubject: true }, (subject, dataTestId) =>
   subject.find(`[data-testid="${dataTestId}"]`),
 );
@@ -27,24 +31,24 @@ declare namespace Cypress {
      * Custom command to intercept GraphQl queries from the KDL.
      * @example cy.kstInterceptor('GetMe', {name: 'Jon Doe'})
      */
-    kstInterceptor(operation: string, responseObject: Object): Chainable<Element>;
+    kstInterceptor(operation: string, responseObject: Object): Chainable;
 
     /**
      * Custom command to get element by data-testid
      * @example cy.getByTestId('my-data-testid')
      */
-    getByTestId(dataTestId: string): Chainable<JQuery<HTMLElement>>;
+    getByTestId(dataTestId: string): Chainable;
 
     /**
      * Custom command to find an element by data-testid
      * @example cy.findByTestId('my-data-testid')
      */
-    findByTestId(dataTestId: string): Chainable<JQuery<HTMLElement>>;
+    findByTestId(dataTestId: string): Chainable;
 
     /**
      * Custom command to open the settings
      * @example cy.openSettings()
      */
-    openSettings(): null;
+    openSettings(): void;
   }
 }

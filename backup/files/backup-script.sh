5c7617f687bb1521cd4e742bdc515e5ae4f6f2e5
Borja Lopez <borja.lopez@intelygenz.com>
2021-12-02 15:33:48 +0100

    fix(backup): Update script

--- backup/files/backup-script.sh
+++ backup/files/backup-script.sh
@@ -4,25 +4,27 @@ DATE=$(date +%Y%m%d)
 mkdir -p /backup/{gitea,mongodb,mlflow,kubernetes}
 
 # Gitea backup
-function gitea_backup (){
+function gitea_backup () {
     echo "${DB_HOST}:${POSTGRES_DB}:${DB_USER}:${POSTGRES_PASSWORD}" > ~/.pgpass
     chmod 600 ~/.pgpass
     tar zcvf /backup/gitea/data.tar.gz --directory '/data' --exclude './gitea/indexers' .
     pg_dump -h "$(echo ${DB_HOST} | cut -f 1 -d ":" )" -p "$(echo ${DB_HOST} | cut -f 2 -d ":" )" -U "${DB_USER}" -v -F c -f /backup/gitea/postgres_gitea.dump "${POSTGRES_DB}"
 }
+
 # MongoDB backup
 function mongobackup () {
     mongodump --host "${MONGO_HOST}" --port "${MONGO_PORT}" --username "${MONGO_INITDB_ROOT_USERNAME}" --password "${MONGO_INITDB_ROOT_PASSWORD}" --out /backup/mongodb
 }
+
 # MLFlow backup
-function mlflow_backup (){
+function mlflow_backup () {
     for i in /shared-volume/*/mlflow.db; do
         path="/backup/mlflow/";
         cp --preserve -R "${i%%mlflow.db}" "${path}"; done
 }
 
 # Kubernetes CR and Secrets backup
-function kubernetes_backup (){
+function kubernetes_backup () {
     kubectl --token "$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" get secrets -n kdl | \
         awk '/ssh-keys/{print $1}' | \
         xargs kubectl --token "$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" get secrets -n kdl -o yaml > /backup/kubernetes/ssh-secrets.yaml
@@ -43,8 +45,10 @@ fi
 echo "Performing KUBERNETES backup..."
 kubernetes_backup
 
-# Compress and send to AWS
+# Compress backup
 echo "Compressing backup..."
 tar zcvf "${HOME}/backup.tar.gz" /backup
-echo "Sending backup..."    
+
+# Send to AWS
+echo "Sending backup to AWS..."    
 aws s3 cp "${HOME}/backup.tar.gz" "s3://${BUCKET_NAME}/backup_${DATE}.tar.gz"

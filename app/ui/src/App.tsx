7737d408cc57eb8bcf628f15b7a37ec2a72469f5
Juan Jesús Padrón Hernández <juanjesus.padron@intelygenz.com>
2022-02-20 19:25:48 +0000

    wip: get kubeconfig page

--- app/ui/src/App.tsx
+++ app/ui/src/App.tsx
@@ -20,6 +20,7 @@ import { GetMe } from './Graphql/queries/types/GetMe';
 import GetMeQuery from './Graphql/queries/getMe';
 import { ErrorMessage, SpinnerCircular } from 'kwc';
 import { AccessLevel } from './Graphql/types/globalTypes';
+import UserKubeconfig from './Pages/UserKubeconfig/UserKubeconfig';
 
 const routesWithTopBar = [ROUTE.USERS, ROUTE.HOME, ROUTE.PROJECT, ROUTE.USER_SSH_KEY];
 
@@ -39,6 +40,7 @@ function Routes() {
 
         {canAccessUsers && <Route exact path={ROUTE.USERS} component={Users} />}
         <Route exact path={ROUTE.USER_SSH_KEY} component={UserSshKey} />
+        <Route exact path={ROUTE.USER_KUBECONFIG} component={UserKubeconfig} />
 
         <Route exact path={ROUTE.NEW_PROJECT} component={NewProject} />
         <Route exact path={ROUTE.PROJECT_CREATION} component={ProjectCreation} />
---
+++ app/ui/src/Pages/UserKubeconfig/Components/Kubeconfig.tsx
@@ -0,0 +1,36 @@
+import { Button } from 'kwc';
+import * as React from 'react';
+import { copyAndToast } from 'Utils/clipboard';
+import styles from './Kubeconfig.module.scss';
+import IconFileCopy from '@material-ui/icons/FileCopy';
+import IconGetApp from '@material-ui/icons/GetApp';
+
+type Props = {
+  kubeconfig: string;
+};
+
+function downloadFile(text: string) {
+  const element = document.createElement('a');
+  const file = new Blob([text], {
+    type: 'text/yaml',
+  });
+  element.href = URL.createObjectURL(file);
+  element.download = 'kubeconfig';
+  document.body.appendChild(element);
+  element.click();
+}
+
+function Kubeconfig({ kubeconfig }: Props) {
+  console.log(kubeconfig);
+  return (
+    <div className={styles.container}>
+      <div className={styles.kubeconfig}>{kubeconfig.trim()}</div>
+      <div className={styles.buttonsContainer}>
+        <Button Icon={IconFileCopy} label="" className={styles.button} onClick={() => copyAndToast(kubeconfig)} />
+        <Button Icon={IconGetApp} label="" className={styles.button} onClick={() => downloadFile(kubeconfig)} />
+      </div>
+    </div>
+  );
+}
+
+export default Kubeconfig;

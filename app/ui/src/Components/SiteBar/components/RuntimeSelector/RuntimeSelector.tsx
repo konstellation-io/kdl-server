3448cf33b357cdcb8e5c660d98e280275cecdd55
Juan Jesús Padrón Hernández <juanjesus.padron@intelygenz.com>
2022-02-21 11:49:53 +0000

    delete unused imports on RuntimeSelector

--- app/ui/src/Components/SiteBar/components/RuntimeSelector/RuntimeSelector.tsx
+++ app/ui/src/Components/SiteBar/components/RuntimeSelector/RuntimeSelector.tsx
@@ -1,11 +1,8 @@
 import React, { FC } from 'react';
 import styles from './RuntimeSelector.module.scss';
-import { NavLink } from 'react-router-dom';
-import ROUTE, { buildRoute } from 'Constants/routes';
 import { BottomComponentProps } from '../Breadcrumbs/components/Crumb/Crumb';
 import { GetRuntimes_runtimes } from 'Graphql/queries/types/GetRuntimes';
 import RuntimeItem from './RuntimeItem/RuntimeItem';
-import { run } from 'apollo';
 
 type Props = {
   runtimes: GetRuntimes_runtimes[];

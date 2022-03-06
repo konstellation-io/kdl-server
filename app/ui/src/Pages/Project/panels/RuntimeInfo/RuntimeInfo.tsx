c8418c31d713fcfacd795319d838c3f198680ff5
Roberto Rodríguez <roberto.rodriguez@intelygenz.com>
2022-03-06 12:19:50 +0100

    Show to the user a loading status when the runtime is not ready jet

--- app/ui/src/Pages/Project/panels/RuntimeInfo/RuntimeInfo.tsx
+++ app/ui/src/Pages/Project/panels/RuntimeInfo/RuntimeInfo.tsx
@@ -3,7 +3,7 @@ import { GetRuntimes_runtimes } from 'Graphql/queries/types/GetRuntimes';
 import styles from './RuntimeInfo.module.scss';
 import LabelList from '../RuntimesList/components/LabelList';
 import { useReactiveVar } from '@apollo/client';
-import { runningRuntime } from 'Graphql/client/cache';
+import { loadingRuntime, runningRuntime } from 'Graphql/client/cache';
 
 type Props = {
   selectedRuntime: GetRuntimes_runtimes;
@@ -12,6 +12,7 @@ type Props = {
 
 function RuntimeInfo({ selectedRuntime: runtime, isKubeconfigEnabled }: Props) {
   const activeRuntime = useReactiveVar(runningRuntime);
+  const runtimeLoading = useReactiveVar(loadingRuntime);
   const running = activeRuntime?.id === runtime.id;
   return (
     <div>
@@ -19,11 +20,15 @@ function RuntimeInfo({ selectedRuntime: runtime, isKubeconfigEnabled }: Props) {
         <div data-testid="runtimeInfoPanel">
           <div className={styles.title}>
             <h1 className={styles.headerName}>{runtime.name}</h1>
-            {running && (
+            {running && !runtimeLoading ?
               <div className={styles.runningTag} data-testid="statusTag">
                 Running
               </div>
-            )}
+              :
+              <div className={styles.loadingTag} data-testid="statusTag">
+                Loading
+              </div>
+            }
           </div>
           <div className={styles.runtimeTags}>
             <LabelList runtime={runtime} />

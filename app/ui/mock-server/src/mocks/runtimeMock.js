194694b2118bd96ad107889c14335a39b98f72fc
Roberto Rodríguez <roberto.rodriguez@intelygenz.com>
2022-03-24 17:40:57 +0100

    fix(ui) removed repeated example lib

--- app/ui/mock-server/src/mocks/runtimeMock.js
+++ app/ui/mock-server/src/mocks/runtimeMock.js
@@ -2,7 +2,7 @@ const casual = require('casual');
 
 casual.define('html', function() {
   let htmlStr = "<p><b>" + casual.sentences(1) + "</b></p>"
-  htmlStr += "<ul><li>TensorFlow 2.8.0</li><li>SpaCy 3.2.3</li>"
+  htmlStr += "<ul><li>TensorFlow 2.8.0</li>"
   htmlStr += "<li>SpaCy 3.2</li>"
   htmlStr += "<li>NLTK 3.7</li></ul>"
   htmlStr += casual.sentences(10);

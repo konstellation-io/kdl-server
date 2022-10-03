c9ee5b8318926caca6003d09dec769cc0eab1a0c
Angel Luis Piquero <angelluis.piquero@intelygenz.com>
2022-10-03 16:50:08 +0200

    fix(helm): MINIO_ENDPOINT variable value from kdl-server-configmap

---
+++ migrate-project.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NAMESPACE=kdl
+RELEASE_NAME=kdl-local
+
+cat << EOF > patch-file.yaml
+spec:
+  minio:
+    endpointURL: http://${RELEASE_NAME}-minio:9000
+EOF
+
+for project in $(kubectl -n ${NAMESPACE} get kdlprojects.project.konstellation.io -o name); do kubectl -n ${NAMESPACE} patch ${project} --type merge --patch-file patch-file.yaml; done
+rm -f patch-file.yaml

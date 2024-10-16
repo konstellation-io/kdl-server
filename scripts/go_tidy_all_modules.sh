e9d9b3efa82f5cc19739052c8dda75982e3a133d
Alex Rodriguez <alexrodfe@gmail.com>
2024-10-16 16:46:31 +0200

    upgrade all go.sum

--- scripts/go_tidy_all_modules.sh
+++ scripts/go_tidy_all_modules.sh
@@ -4,16 +4,20 @@ echo 'Go mod tidy all modules'
 
 echo 'Upgrading go dependencies for app/api'
 cd app/api
+rm go.sum
 go mod tidy
 
 echo 'Upgrading go dependencies for cleaner'
 cd ../../cleaner
+rm go.sum
 go mod tidy
 
 echo 'Upgrading go dependencies for gitea-oauth2-setup'
 cd ../gitea-oauth2-setup
+rm go.sum
 go mod tidy
 
 echo 'Upgrading go dependencies for repo-cloner'
 cd ../repo-cloner
+rm go.sum
 go mod tidy

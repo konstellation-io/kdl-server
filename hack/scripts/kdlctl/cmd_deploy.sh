9b5d1b7ecb88cc80122e29c31a744586eac45e59
Iván Alejandro Marugán <ivan.alejandro@intelygenz.com>
2024-12-16 12:18:42 +0100

    chore(hack): [KDL6-224] prepare MacOS deployment (#1084)
    chore(hack): clean legacy values
    
    * Clean useless bash function
    * Remove useless ENV
    * Change MongoDB by mongodb-operator
    * Centralize values

--- hack/scripts/kdlctl/cmd_deploy.sh
+++ hack/scripts/kdlctl/cmd_deploy.sh
@@ -27,38 +27,18 @@ show_deploy_help() {
 deploy() {
   export DOMAIN=kdl.$HOST_IP.nip.io
 
-  prepare_helm
-
   if [ "$BUILD_DOCKER_IMAGES" = "1" ]; then
     build_docker_images
   fi
-
-  create_namespace
 
   ./scripts/create_nginx_ingress_configmap.sh
 
   deploy_helm_chart
 }
 
-HELM_READY=""
-
-prepare_helm() {
-  if [ "$HELM_READY" = "1" ]; then
-    return
-  fi
-
-  if [ "$HELM_VERSION" = "2" ]; then
-    # Helm v2 needs to be initiated first
-    echo_wait "Init helm v2..."
-    run helm init --upgrade --wait
-  fi
-
-  HELM_READY=1
-}
-
 get_kubectl_dry_run() {
-    act_version="$(kubectl version --client=true --short | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')"
-    req_version="1.18.0"
+    act_version="$(kubectl version --client=true | sed 's/[^0-9.]*\([0-9.]*\).*/\1/' | head -1)"
+    req_version="1.30.0"
 
     # get the lowest version of the two compared
     lowest_version=$(printf '%s\n' "${act_version}" "${req_version}" | sort -V | head -n1)
@@ -72,26 +52,12 @@ get_kubectl_dry_run() {
     echo "--dry-run"
 }
 
-create_namespace() {
-  microk8s_kubeconfig
-
-  DRY_RUN=$(get_kubectl_dry_run)
-  echo_info "📚️ Create Namespace if not exist..."
-  NS=$(kubectl create ns "${NAMESPACE}" ${DRY_RUN} -o yaml)
-  if [ "$VERBOSE" = "1" ]; then
-    # NOTE: there is no way to call run() with pipe commands
-    echo_run "kubectl create ns \"${NAMESPACE}\" ${DRY_RUN} -o yaml | kubectl apply -f -"
-    echo "$NS" | kubectl apply -f -
-  else
-    echo "$NS" | kubectl apply > /dev/null -f - 2>&1
-  fi
-}
-
 deploy_helm_chart() {
+  microk8s_kubeconfig
 
   export KNOWLEDGE_GALAXY_IMAGE_REPOSITORY="konstellation/knowledge-galaxy"
   if [ "$KNOWLEDGE_GALAXY_LOCAL" = "true"  ]; then
-    export KNOWLEDGE_GALAXY_IMAGE_REPOSITORY="$IMAGE_REGISTRY/konstellation/knowledge-galaxy"
+    export KNOWLEDGE_GALAXY_IMAGE_REPOSITORY="$IMAGE_REGISTRY/konstellation/kdl-knowledge-galaxy"
     export KNOWLEDGE_GALAXY_IMAGE_TAG="latest"
     echo_info "LOCAL KG"
   fi

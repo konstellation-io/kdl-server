074183e8846676e711fea695f3bd5c5b7e51ff6b
joan.carballo <joan.carballo@intelygenz.com>
2025-01-09 08:46:37 +0100

    Fix URL check value

--- app/ui/src/Pages/NewProject/pages/RepositoryDetails/RepositoryDetailsUtils.ts
+++ app/ui/src/Pages/NewProject/pages/RepositoryDetails/RepositoryDetailsUtils.ts
@@ -2,7 +2,7 @@ import { CHECK } from 'kwc';
 
 export function validateUrl(value: string) {
   console.log('Validating URL: ', value);
-  return CHECK.getValidationError([CHECK.isFieldNotEmpty(value), CHECK.isUrlValid(new URL(value))]);
+  return CHECK.getValidationError([CHECK.isFieldNotEmpty(value), CHECK.isUrlValid(value)]);
 }
 
 export function validateMandatoryField(value: string) {

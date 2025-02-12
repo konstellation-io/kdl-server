c747d52f825532b3990dc6df58acb659af18ab00
Javier Gil <javier.gilm@intelygenz.com>
2025-02-12 09:11:24 +0100

    chore: upgrade kdl-local version in helm chart (#1161)
    Upgrade hack script to remove kdlusertools and kdlprojects

--- hack/scripts/kdlctl/cmd_uninstall.sh
+++ hack/scripts/kdlctl/cmd_uninstall.sh
@@ -9,11 +9,18 @@ cmd_uninstall() {
       echo_info "Deleting \"$RELEASE_NAME\" helm release..."
       helmfile -f scripts/helmfile/helmfile.yaml delete
 
-      echo_info "Deleting usertools in \"$NAMESPACE\" k8s namespace..."
-      USERTOOL_NAMES=$(kubectl get usertools -n kdl | tail -n +2 | awk '{print $1}')
-      for NAME in $USERTOOL_NAMES; do
-        echo_info "   - Deleting \"$NAME\" usertool finalizer..."
-        kubectl -n ${NAMESPACE} patch usertools $NAME --type json --patch='[ { "op": "remove", "path": "/metadata/finalizers" } ]'
+      echo_info "Deleting kdlusertools in \"$NAMESPACE\" k8s namespace..."
+      KDLUSERTOOL_NAMES=$(kubectl get kdlusertools -n kdl | tail -n +2 | awk '{print $1}')
+      for NAME in $KDLUSERTOOL_NAMES; do
+        echo_info "   - Deleting \"$NAME\" kdlusertools finalizer..."
+        kubectl -n ${NAMESPACE} patch kdlusertools $NAME --type json --patch='[ { "op": "remove", "path": "/metadata/finalizers" } ]'
+      done
+
+      echo_info "Deleting kdlprojects in \"$NAMESPACE\" k8s namespace..."
+      KDLPROJECT_NAMES=$(kubectl get kdlprojects -n kdl | tail -n +2 | awk '{print $1}')
+      for NAME in $KDLPROJECT_NAMES; do
+        echo_info "   - Deleting \"$NAME\" kdlprojects finalizer..."
+        kubectl -n ${NAMESPACE} patch kdlprojects $NAME --type json --patch='[ { "op": "remove", "path": "/metadata/finalizers" } ]'
       done
 
       echo_info "Deleting \"$NAMESPACE\" k8s namespace..."

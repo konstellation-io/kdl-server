f8c77d26760d72758383954697aaa6c77169ba26
Javier Gil <javier.gil@intelygenz.com>
2025-02-19 17:59:58 +0100

    feat(app): [KDL6-324] add watcher configmap enable flag

--- repo-cloner/config/config_test.go
+++ repo-cloner/config/config_test.go
@@ -18,26 +18,80 @@ func TestConfigSuite(t *testing.T) {
 	suite.Run(t, new(configTestSuite))
 }
 
-func (s *configTestSuite) TestConfig() {
-	// Set the env vars
-	var envs = map[string]string{
-		"KDL_SERVER_MONGODB_URI":  "mongodb_uri",
-		"DB_NAME":                 "db_name",
-		"KDL_USER_NAME":           "user_name",
-		"REPOS_PATH":              "repos_path",
-		"PEM_FILE":                "pem_file",
-		"PEM_FILE_PASSWORD":       "pem_file_password",
-		"CHECK_FREQUENCY_SECONDS": "20",
-	}
+var mandatoryEnv = map[string]string{
+	"KDL_SERVER_MONGODB_URI": "mongodb_uri",
+	"KDL_USER_NAME":          "user_name",
+}
 
-	for k, v := range envs {
+var optionalEnv = map[string]string{
+	"PEM_FILE_PASSWORD": "pem_file_password",
+}
+
+var defaultEnv = map[string]string{
+	"DB_NAME":                 "db_name",
+	"REPOS_PATH":              "repos_path",
+	"PEM_FILE":                "pem_file",
+	"CHECK_FREQUENCY_SECONDS": "20",
+}
+
+const (
+	defaultMongoDBName = "kdl"
+	defaultReposPath   = "/home/kdl/repos/"
+	defaultPemFile     = "/home/kdl/.ssh/id_rsa"
+	defaultCheckFreq   = 10
+)
+
+func (s *configTestSuite) setEnvVariables(envMap map[string]string) {
+	for k, v := range envMap {
 		_ = os.Setenv(k, v)
 	}
+}
 
+func (s *configTestSuite) unsetEnvVariables(envMap map[string]string) {
+	for k := range envMap {
+		_ = os.Unsetenv(k)
+	}
+}
+
+func (s *configTestSuite) TestConfig_MandatoryValues() {
+	// Set mandatoryEnv vars
+	s.setEnvVariables(mandatoryEnv)
+
+	// Act
 	cfg := config.NewConfig()
 
-	// Check the values
+	// Assert
 	s.Require().NotNil(cfg)
+
+	// Assert default values
+	s.Require().Equal(defaultMongoDBName, cfg.MongoDB.DBName)
+	s.Require().Equal(defaultReposPath, cfg.ReposPath)
+	s.Require().Equal(defaultPemFile, cfg.PemFile)
+	s.Require().Equal(defaultCheckFreq, cfg.CheckFrequencySeconds)
+
+	// Assert optional values
+	s.Require().Empty(cfg.PemFilePassword)
+
+	// Assert rest of values
+	s.Require().Equal("mongodb_uri", cfg.MongoDB.URI)
+	s.Require().Equal("user_name", cfg.UsrName)
+
+	// Unset the mandatoryEnv vars
+	s.unsetEnvVariables(mandatoryEnv)
+}
+
+func (s *configTestSuite) TestConfig_SetAllValues() {
+	// Set mandatoryEnv, defaultEnv and optionalEnv vars
+	s.setEnvVariables(mandatoryEnv)
+	s.setEnvVariables(defaultEnv)
+	s.setEnvVariables(optionalEnv)
+
+	// Act
+	cfg := config.NewConfig()
+
+	// Assert
+	s.Require().NotNil(cfg)
+	// Assert values
 	s.Require().Equal("mongodb_uri", cfg.MongoDB.URI)
 	s.Require().Equal("db_name", cfg.MongoDB.DBName)
 	s.Require().Equal("user_name", cfg.UsrName)
@@ -46,39 +100,10 @@ func (s *configTestSuite) TestConfig() {
 	s.Require().Equal("pem_file_password", cfg.PemFilePassword)
 	s.Require().Equal(20, cfg.CheckFrequencySeconds)
 
-	// Unset the env vars
-	for k := range envs {
-		_ = os.Unsetenv(k)
-	}
-}
-
-func (s *configTestSuite) TestConfig_DefaultOptionalValues() {
-	// Set the env vars without a default values or not optional
-	var envs = map[string]string{
-		"KDL_SERVER_MONGODB_URI": "mongodb_uri",
-		"KDL_USER_NAME":          "user_name",
-	}
-
-	for k, v := range envs {
-		_ = os.Setenv(k, v)
-	}
-
-	cfg := config.NewConfig()
-
-	// Check the values
-	s.Require().NotNil(cfg)
-	s.Require().Equal("mongodb_uri", cfg.MongoDB.URI)
-	s.Require().Equal("kdl", cfg.MongoDB.DBName)
-	s.Require().Equal("user_name", cfg.UsrName)
-	s.Require().Equal("/home/kdl/repos/", cfg.ReposPath)
-	s.Require().Equal("/home/kdl/.ssh/id_rsa", cfg.PemFile)
-	s.Require().Equal("", cfg.PemFilePassword)
-	s.Require().Equal(10, cfg.CheckFrequencySeconds)
-
-	// Unset the env vars
-	for k := range envs {
-		_ = os.Unsetenv(k)
-	}
+	// Unset the mandatoryEnv, defaultEnv and optionalEnv vars
+	s.unsetEnvVariables(mandatoryEnv)
+	s.unsetEnvVariables(defaultEnv)
+	s.unsetEnvVariables(optionalEnv)
 }
 
 func (s *configTestSuite) TestConfig_MissingEnvironment() {
